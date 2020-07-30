8be896bd7a17026fb082f191bb7cfb7d323db917
Benjamin Milligan <bmilliga@uoguelph.ca>
2020-07-30 11:48:56 -0400

    Organization

--- examples/Teensy_SDIO_UM7/Datasets.h
+++ examples/Teensy_SDIO_UM7/Datasets.h
@@ -1,9 +1,9 @@
 // Avoid IDE problems by defining struct in septate .h file.
 // Pad record so size is a power of two for best write performance.
-#ifndef Logger_h
-#define Logger_h
+#ifndef Datasets_h
+#define Datasets_h
 const size_t ADC_COUNT = 4;
 struct data_t {
 	uint16_t adc[ADC_COUNT];
 };
-#endif  // Logger_h
+#endif  // Datasets_h
---
+++ examples/Teensy_SDIO_UM7/Parameters.h
@@ -0,0 +1,40 @@
+#ifndef Parameters_h
+#define Parameters_h
+
+#ifndef SDCARD_SS_PIN
+const uint8_t SD_CS_PIN = SS;
+#else  // SDCARD_SS_PIN
+// Assume built-in SD is used.
+const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
+#endif  // SDCARD_SS_PIN
+
+// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
+// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
+#define SD_FAT_TYPE 3
+
+// 32 KiB buffer.
+const size_t BUF_DIM = 32768;
+
+// 8 MiB file.
+const uint32_t FILE_SIZE = 256UL * BUF_DIM;
+
+#if SD_FAT_TYPE == 0
+SdFat sd;
+File file;
+#elif SD_FAT_TYPE == 1
+SdFat32 sd;
+File32 file;
+#elif SD_FAT_TYPE == 2
+SdExFat sd;
+ExFile file;
+#elif SD_FAT_TYPE == 3
+SdFs sd;
+FsFile file;
+#else  // SD_FAT_TYPE
+#error Invalid SD_FAT_TYPE
+#endif  // SD_FAT_TYPE
+
+uint8_t buf[BUF_DIM];
+
+// buffer as uint32_t
+uint32_t* buf32 = (uint32_t*)buf;
