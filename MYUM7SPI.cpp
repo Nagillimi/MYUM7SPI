2b6a969b8ea961525c4f61ef857dcd70d0696026
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2020-08-01 18:21:44 -0400

    Update MYUM7SPI.cpp
    Worked locally and solved the compiler issues

--- MYUM7SPI.cpp
+++ MYUM7SPI.cpp
@@ -6,42 +6,50 @@ Ben Milligan, 2020
 Compilation code from several libraries. Credit to:
  - Michael Hoyer, https://github.com/mikehoyer/UM7-Arduino
  - crazyFrg, https://forum.arduino.cc/index.php?topic=625401.0
- - 
 
  Adaptation to the SPI library to allow for synchronous capabilities.
- Could add:
- - SPISettings
- - health packet/error decode
- - baud rate configs
+
+ Tested on:
+ - Teensy LC
+ - 
+
+ Notes:
+ 1. Since I can't pass an SPI bus for initialization, I've set the 
+    SPI bus used for the sensor as SPI1. SPI0 (or just SPI) is being
+	used by the SD card. This may be a non-issue once I get the SDIO 
+	code working.
+	To set the specific pins for the SPI bus, call SPI1.setMOSI(#),
+	SPI1.setMISO(#), SPI1.setSCK(#) after SPI1.begin().
+ 2. I've kept the binary read functions for now, may still be useful
+    to read direct binary from the sensor for efficiency reasons.
 */
 
 #include "MYUM7SPI.h"
 
-
-
-/*
-	Default constructor. Initializes cs pin and sets it as an output.
-*/
+// Default constructor. Initializes cs pin and sets it as an output.
 MYUM7SPI::MYUM7SPI(uint16_t cs_) {
 	cs = cs_;
 	pinMode(cs, OUTPUT);
 }
 
-/*
-
-*/
+// Useful for combining uint32_t with their composite bytes
+// I realize I can just do bitwise operaions, but this is less code
 typedef union {
 	uint32_t val;
-	uint8_t bytes[4];
+	byte bytes[4];
 } intval;
+
+// Useful for combining floats with their composite bytes
+typedef union {
+	float val;
+	byte bytes[4];
+} floatval;
 
 //////////////////////////////////
 //		CONFIG FUNCTIONS		//
 //////////////////////////////////
 
-/*
-	Sets the rate for all raw datasets. rate will vary from 0-255
-*/
+// Sets the rate for all raw datasets. rate will vary from 0-255
 void MYUM7SPI::set_all_raw_rate(byte rate_) {
 	intval rate;
 	rate.bytes[0] = 0;
@@ -52,9 +60,7 @@ void MYUM7SPI::set_all_raw_rate(byte rate_) {
 	write_register(CREG_COM_RATES2, rate.val);
 }
 
-/*
-	Sets the rate for all processed datasets. rate will vary from 0-255
-*/
+// Sets the rate for all processed datasets. rate will vary from 0-255
 void MYUM7SPI::set_all_processed_rate(byte rate_) {
 	intval rate;
 	rate.bytes[0] = 0;
@@ -65,13 +71,11 @@ void MYUM7SPI::set_all_processed_rate(byte rate_) {
 	write_register(CREG_COM_RATES4, rate.val);
 }
 
-/*
-	Overloaded function for multiple rate config capabilities. includes the:
-	- quaternion rate
-	- euler rate
-	- position rate
-	- velocity rate
-*/
+// Overloaded function for multiple rate config capabilities. includes the:
+// - quaternion rate
+// - euler rate
+// - position rate
+// - velocity rate
 void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate, byte pos_rate, byte vel_rate) {
 	intval rate;
 	rate.bytes[0] = quat_rate;
@@ -82,13 +86,10 @@ void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate, byte pos_ra
 	write_register(CREG_COM_RATES4, rate.val);
 }
 
-/*
-	Overloaded function for multiple rate config capabilities. includes the:
-	- quaternion rate
-	- euler rate
-	- position rate
-	- velocity rate
-*/
+// Overloaded function for multiple rate config capabilities. includes the:
+// - quaternion rate
+// - euler rate
+// - position rate
 void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate, byte pos_rate) {
 	intval rate;
 	rate.bytes[0] = quat_rate;
@@ -99,13 +100,9 @@ void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate, byte pos_ra
 	write_register(CREG_COM_RATES4, rate.val);
 }
 
-/*
-	Overloaded function for multiple rate config capabilities. includes the:
-	- quaternion rate
-	- euler rate
-	- position rate
-	- velocity rate
-*/
+// Overloaded function for multiple rate config capabilities. includes the:
+// - quaternion rate
+// - euler rate
 void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate) {
 	intval rate;
 	rate.bytes[0] = quat_rate;
@@ -116,13 +113,8 @@ void MYUM7SPI::set_orientation_rate(byte quat_rate, byte euler_rate) {
 	write_register(CREG_COM_RATES4, rate.val);
 }
 
-/*
-	Overloaded function for multiple rate config capabilities. includes the:
-	- quaternion rate
-	- euler rate
-	- position rate
-	- velocity rate
-*/
+// Overloaded function for multiple rate config capabilities. includes the:
+// - quaternion rate
 void MYUM7SPI::set_orientation_rate(byte quat_rate) {
 	intval rate;
 	rate.bytes[0] = quat_rate;
@@ -133,15 +125,13 @@ void MYUM7SPI::set_orientation_rate(byte quat_rate) {
 	write_register(CREG_COM_RATES4, rate.val);
 }
 
-/*
-	Miscellaneous settings for filter and sensor control options. Send a 0 if you don't wish to configure a specific setting
-	Ex. set_misc_settings(0, 1, 1, 0)
-
-	PPS bit = Causes the TX2/RX2 pin to be used with an external GPS
-	ZG bit = Causes UM7 to measure gyro bias at setup
-	Q bit = Sensor will run in Quternion mode instead of Euler mode. Fixes pitch error in the Gimbal lock position
-	MAG bit = Magnetometer will be used in state updates
-*/
+// Miscellaneous settings for filter and sensor control options. Send a 0 if you don't wish to configure a specific setting
+// Ex. set_misc_settings(0, 1, 1, 0)
+//
+// PPS bit = Causes the TX2/RX2 pin to be used with an external GPS
+// ZG bit = Causes UM7 to measure gyro bias at setup
+// Q bit = Sensor will run in Quternion mode instead of Euler mode. Fixes pitch error in the Gimbal lock position
+// MAG bit = Magnetometer will be used in state updates
 void MYUM7SPI::set_misc_ssettings(bool pps, bool zg, bool q, bool mag) {
 	byte b1 = 0, b0 = 0;
 
@@ -174,6 +164,7 @@ void MYUM7SPI::set_misc_ssettings(bool pps, bool zg, bool q, bool mag) {
 //		DATA FUNCTIONS		//
 //////////////////////////////
 
+// Assigns all raw (gyro, accel, mag) data from the associated registers
 void MYUM7SPI::get_all_raw_data() {
 	gyro_raw_x = read_register(DREG_GYRO_RAW_XY, 1);
 	gyro_raw_y = read_register(DREG_GYRO_RAW_XY, 0);
@@ -194,6 +185,7 @@ void MYUM7SPI::get_all_raw_data() {
 	temp_time = read_register(DREG_TEMPERATURE_TIME);
 }
 
+// Assigns all processed (gyro, accel, mag) data from the associated registers
 void MYUM7SPI::get_all_processed_data() {
 	gyro_x = read_register(DREG_GYRO_PROC_X);
 	gyro_y = read_register(DREG_GYRO_PROC_Y);
@@ -211,6 +203,7 @@ void MYUM7SPI::get_all_processed_data() {
 	mag_time = read_register(DREG_MAG_PROC_TIME);
 }
 
+// Assigns all orientation data from the associated registers
 void MYUM7SPI::get_all_orientation_data() {
 	quat_a = read_register(DREG_QUAT_AB, 1) / 29789.09091;
 	quat_b = read_register(DREG_QUAT_AB, 0) / 29789.09091;
@@ -237,6 +230,7 @@ void MYUM7SPI::get_all_orientation_data() {
 	vel_time = read_register(DREG_VELOCITY_TIME);
 }
 
+// Custom read function for Val's datasets
 void MYUM7SPI::get_vals_data() {
 	gyro_x = read_register(DREG_GYRO_PROC_X);
 	gyro_y = read_register(DREG_GYRO_PROC_Y);
@@ -249,71 +243,50 @@ void MYUM7SPI::get_vals_data() {
 	roll = read_register(DREG_EULER_PHI_THETA, 1) / 91.02222;
 	pitch = read_register(DREG_EULER_PHI_THETA, 0) / 91.02222;
 	yaw = read_register(DREG_EULER_PSI, 1) / 91.02222;
-
-	// Include these datasets to get to 128 B/transfer
-	roll_rate = read_register(DREG_EULER_PHI_THETA_DOT, 1) / 16.0;
-	pitch_rate = read_register(DREG_EULER_PHI_THETA_DOT, 0) / 16.0;
-	yaw_rate = read_register(DREG_EULER_PSI_DOT, 1) / 16.0;
 }
 
 //////////////////////////////////
 //		COMMAND FUNCTIONS		//
 //////////////////////////////////
 
-/*
-	Causes the UM7 to load default factory settings.
-*/
+// Causes the UM7 to load default factory settings.
 int32_t MYUM7SPI::get_firmware() {
 	return read_register(GET_FW_REVISION);
 }
 
-/*
-	Causes the UM7 to load default factory settings.
-*/
+// Causes the UM7 to load default factory settings.
 void MYUM7SPI::flash_commit() {
 	write_register(FLASH_COMMIT);
 }
 
-/*
-	Causes the UM7 to load default factory settings.
-*/
+// Causes the UM7 to load default factory settings.
 void MYUM7SPI::factory_reset() {
 	write_register(RESET_TO_FACTORY);
 }
 
-/*
-	Causes the UM7 to measure the gyro outputs and set the output trim registers to compensate for any non-zero bias. 
-	The UM7 should be kept stationary while the zero operation is underway.
-*/
+// Causes the UM7 to measure the gyro outputs and set the output trim registers to compensate for any non-zero bias. 
+// The UM7 should be kept stationary while the zero operation is underway.
 void MYUM7SPI::zero_gyros() {
 	write_register(ZERO_GYROS);
 }
 
-/*
-	Sets the current GPS latitude, longitude, and altitude as the home position. 
-	All future positions will be referenced to the current GPS position.
-*/
+// Sets the current GPS latitude, longitude, and altitude as the home position. 
+// All future positions will be referenced to the current GPS position.
 void MYUM7SPI::set_home_position() {
 	write_register(SET_HOME_POSITION);
 }
 
-/*
-	Sets the current yaw heading position as north.
-*/
+// Sets the current yaw heading position as north.
 void MYUM7SPI::set_mag_reference() {
 	write_register(SET_MAG_REFERENCE);
 }
 
-/*
-	Reboots the UM7 and performs a crude calibration on the accelerometers. Best performed on a flat surface.
-*/
+// Reboots the UM7 and performs a crude calibration on the accelerometers. Best performed on a flat surface.
 void MYUM7SPI::calibrate_accelerometers() {
 	write_register(CALIBRATE_ACCELEROMETERS);
 }
 
-/*
-	Resets the Extended Kalman Filter (EKF)
-*/
+// Resets the Extended Kalman Filter (EKF)
 void MYUM7SPI::reset_ekf() {
 	write_register(RESET_EKF);
 }
@@ -322,34 +295,33 @@ void MYUM7SPI::reset_ekf() {
 //		INTERNAL FUNCTIONS		//
 //////////////////////////////////
 
-/*
-	Read a register that carries 2 datasets (euler data). Uses a user defined bool to determine which dataset to return
-*/
+// Read a register that carries 2 datasets (euler data). 
+// Uses a user defined bool to determine which dataset to return
 int16_t MYUM7SPI::read_register(byte address, bool first_half) {
 	byte inByte = 0;
 	int16_t result;
 	
 	digitalWrite(cs, LOW);
 	
-	SPI.transfer(READ);
+	SPI1.transfer(READ);
 	delayMicroseconds(5);
 	
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 	
 	if(!first_half) {
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 	}
-	result = SPI.transfer(0x00);
+	result = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
 	result = result << 8;
 
-	inByte = SPI.transfer(0x00);
+	inByte = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
 	result = result | inByte;
@@ -358,37 +330,29 @@ int16_t MYUM7SPI::read_register(byte address, bool first_half) {
 	return(result);
 }
 
-/*
-	Read from a register. Assume register takes an entire 4 Bytes and is a float point type.
-*/
-int32_t MYUM7SPI::read_register(byte address) {
-	byte inByte = 0;
-	int32_t result;
+// Read from a register. Assume register takes an entire 4 Bytes and is a float point type.
+float MYUM7SPI::read_register(byte address) {
+	floatval result;
 
 	digitalWrite(cs, LOW);
 
-	SPI.transfer(READ);
+	SPI1.transfer(READ);
 	delayMicroseconds(5);
 
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 
-	result = SPI.transfer(0x00);
+	result.bytes[3] = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
-	for (int i = 0; i < 3; i++) {
-		result = result << 8;
-
-		inByte = SPI.transfer(0x00);
+	for (int i = 2; i >= 0; i--) {
+		result.bytes[i] = SPI1.transfer(0x00);
 		delayMicroseconds(5);
-
-		result = result | inByte;
 	}
 
 	digitalWrite(cs, HIGH);
-	return(result);
+	return(result.val);
 }
-
 
 // Used for the SD example in order to write binary data directly,
 // conversion to a csv file is done after data collection. 
@@ -397,22 +361,22 @@ int32_t MYUM7SPI::read_register(byte address) {
 void MYUM7SPI::read_binary_data(byte address, byte b0, byte b1, byte b2, byte b3) {
 	digitalWrite(cs, LOW);
 
-	SPI.transfer(READ);
+	SPI1.transfer(READ);
 	delayMicroseconds(5);
 
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 
-	b3 = SPI.transfer(0x00);
+	b3 = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
-	b2 = SPI.transfer(0x00);
+	b2 = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
-	b1 = SPI.transfer(0x00);
+	b1 = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
-	b0 = SPI.transfer(0x00);
+	b0 = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
 	digitalWrite(cs, HIGH);
@@ -425,20 +389,20 @@ void MYUM7SPI::read_binary_data(byte address, byte b0, byte b1, byte b2, byte b3
 void MYUM7SPI::read_binary_data(byte address, byte b0, byte b1, bool first_half) {
 	digitalWrite(cs, LOW);
 
-	SPI.transfer(READ);
+	SPI1.transfer(READ);
 	delayMicroseconds(5);
 
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 
 	if (!first_half) {
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 	}
-	b1 = SPI.transfer(0x00);
+	b1 = SPI1.transfer(0x00);
 	delayMicroseconds(5);
 
 	b0 = SPI.transfer(0x00);
@@ -447,10 +411,10 @@ void MYUM7SPI::read_binary_data(byte address, byte b0, byte b1, bool first_half)
 	// Got some weird warning about invoking undefined behaviour 
 	// if you stop reading part way through a register, so this is added
 	if (first_half) {
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 	}
 
@@ -458,38 +422,41 @@ void MYUM7SPI::read_binary_data(byte address, byte b0, byte b1, bool first_half)
 }
 
 // Writes to a configuration register, sends the contents of "contents_" to the proper bytes.
+// This is an overloaded function with dual calls for command and configuration writes()
 void MYUM7SPI::write_register(byte address, uint32_t contents_) {
 	intval contents;
 	contents.val = contents_;
 
 	digitalWrite(cs, LOW);
 
-	SPI.transfer(WRITE);
+	SPI1.transfer(WRITE);
 	delayMicroseconds(5);
 
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 
 	for (int i = 3; i >= 0; i--) {
-		SPI.transfer(contents.bytes[i]);
+		SPI1.transfer(contents.bytes[i]);
 		delayMicroseconds(5);
 	}
 
 	digitalWrite(cs, HIGH);
 }
 
-// Writes to a command register. Since no contents are required, the SPI bus passes 0x00 over the MOSI line.
+// Writes to a command register. Since no contents are required, 
+// the SPI bus passes 0x00 over the MOSI line.
+// This is an overloaded function with dual calls for command and configuration writes()
 void MYUM7SPI::write_register(byte address) {
 	digitalWrite(cs, LOW);
 
-	SPI.transfer(WRITE);
+	SPI1.transfer(WRITE);
 	delayMicroseconds(5);
 
-	SPI.transfer(address);
+	SPI1.transfer(address);
 	delayMicroseconds(5);
 
 	for (int i = 0; i < 4; i++) {
-		SPI.transfer(0x00);
+		SPI1.transfer(0x00);
 		delayMicroseconds(5);
 	}
 
