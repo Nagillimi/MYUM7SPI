e52bcb6a8a4755d7954c1ca9ed5d16d371c38a91
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2020-09-15 07:26:30 -0400

    Update ExFatLogger.h

--- examples/Teensy_DEDICATED_SPI_UM7/ExFatLogger.h
+++ examples/Teensy_DEDICATED_SPI_UM7/ExFatLogger.h
@@ -16,9 +16,10 @@
 
 #include "MYUM7SPI.h"
 
-MYUM7SPI imu1(6); // cs pin 1
-MYUM7SPI imu2(9); // cs pin 2
-MYUM7SPI imu3(4); // cs pin 3
+// Init um7s at 10MHz (max)
+MYUM7SPI imu1(6, 10000000); // cs pin 1
+MYUM7SPI imu2(9, 10000000); // cs pin 2
+MYUM7SPI imu3(4, 10000000); // cs pin 3
 
 #define UM7_MOSI_PIN 11
 #define UM7_MISO_PIN 12
