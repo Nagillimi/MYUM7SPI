02f91c2e7309ffc882f3f7e26f1ea6da6616ebe9
Ben Milligan <32855321+Nagillimi@users.noreply.github.com>
2020-09-27 15:03:54 -0400

    Create Individual_Teensys.ino

---
+++ examples/Individual_Teensys/Individual_Teensys.ino
@@ -0,0 +1,475 @@
+/* Arduino example for a Teensy Low Latency Logger using DEDICATED SPI
+   and formatting the SD card as exFAT
+   
+   Ben Milligan, September 2020
+  
+   An implementation of the UM7's SPI mode to allow for logging of 
+   1 UM7 @ 250 Hz (will duplicate data) and 2 FSR's @ 500 Hz
+   (latency of 2000 usec)into Bill Greiman's SdFat-beta library 
+   ExFatLogger example. Thanks Bill!!
+
+   This example overcomes the overflow errors seen in longer SPI transmissions 
+   by using multiple Teensy controllers set at the imus. The power and start
+   button cables will be passed between each, allowing a synchronous start
+   time between all controllers.
+   
+   Tested on:
+   - [120MHz] Teensy 3.5, SPI only! Hardware fault in SDIO mode for both 3.5/6
+   - [180MHz] Teensy 3.6, SPI only! Hardware fault in SDIO mode for both 3.5/6
+   
+   Notes:
+   1. You need to format your SD card as exFAT before this example works,
+      otherwise you can change SD_FAT_TYPE to match your SD partition.
+   2. Should use the built-in bin_to_csv function since it calculates and
+      displays missed packets
+   3. If you'd like to run a CRC (cyclic redundancy check) on the data while 
+      writing, change USE_SD_CRC to non-zero in SdFatConfig.h (~line 181). 
+      Doesn't hinder rate.
+ */
+#include "SdFat.h"
+#include "FreeStack.h"
+#include "Parameters.h"
+//=============================USER FUNCTIONS===================================
+// Replace logRecord(), printRecord(), and ExFatLogger.h for your sensors.
+void logRecord(data_t* data) {
+  data->t = (micros() - t0);
+  data->fsr_heel = analogRead(fsr_heel_pin);
+  data->fsr_toe = analogRead(fsr_toe_pin);
+  imu1.get_vals_data();
+  data->gx_1 = imu1.gyro_x;
+  data->gy_1 = imu1.gyro_y;
+  data->gz_1 = imu1.gyro_z;
+  data->ax_1 = imu1.accel_x;
+  data->ay_1 = imu1.accel_y;
+  data->az_1 = imu1.accel_z;
+  data->roll_1 = imu1.roll;
+  data->pitch_1 = imu1.pitch;
+  data->yaw_1 = imu1.yaw;
+}
+//------------------------------------------------------------------------------
+void printRecord(Print* pr, data_t* data, bool test_) {
+  static uint32_t nr = 0;
+
+  // Print data header when no data is sent to printRecord()
+  if (!data) {
+    // File info lines
+    pr->print(F("LOG INTERVAL,"));
+    pr->print(LOG_INTERVAL_USEC);
+    pr->print(F(",microseconds"));
+    pr->println();
+    pr->print(F("TOTAL LOG TIME,"));
+    pr->print(log_time);
+    pr->print(F(",seconds"));
+    pr->println();
+
+    // Dataset Titles
+    pr->print(F("TRANSFER #"));
+    pr->print(F(",TIME"));
+    pr->print(F(",TIME DELTA"));
+    pr->print(F(",FSR HEEL"));
+    pr->print(F(",FSR TOE"));
+    pr->print(F(",G1X"));
+    pr->print(F(",G1Y"));
+    pr->print(F(",G1Z"));
+    pr->print(F(",A1X"));
+    pr->print(F(",A1Y"));
+    pr->print(F(",A1Z"));
+    pr->print(F(",ROLL1"));
+    pr->print(F(",PITCH1"));
+    pr->print(F(",YAW1"));
+    pr->println();
+    nr = 0;
+    return;
+  }
+
+  // Test if the delta is too high
+  if (data->t - delta >= MAX_INTERVAL_USEC && !test_) {
+    pr->print(F("Missed Packet(s)\n"));
+  }
+  
+  // Print data packet
+  pr->print(nr++);
+  pr->write(','); pr->print(data->t);
+  pr->write(','); pr->print(data->t - delta);
+  pr->write(','); pr->print(data->fsr_heel);
+  pr->write(','); pr->print(data->fsr_toe);
+  pr->write(','); pr->print(data->gx_1);
+  pr->write(','); pr->print(data->gy_1);
+  pr->write(','); pr->print(data->gz_1);
+  pr->write(','); pr->print(data->ax_1);
+  pr->write(','); pr->print(data->ay_1);
+  pr->write(','); pr->print(data->az_1);
+  pr->write(','); pr->print(data->roll_1);
+  pr->write(','); pr->print(data->pitch_1);
+  pr->write(','); pr->print(data->yaw_1);
+  pr->println();
+
+  // Reset delta to hold time for the next packet
+  delta = data->t;
+}
+//==============================================================================
+//------------------------------------------------------------------------------
+#define error(s) sd.errorHalt(&Serial, F(s))
+#define dbgAssert(e) ((e) ? (void)0 : error("assert " #e))
+//------------------------------------------------------------------------------
+// Convert binary file to csv file.
+void binaryToCsv() {
+  Serial.println(F("Converting binary data..."));
+  uint8_t lastPct = 0;
+  uint32_t start = millis();
+  data_t binData[FIFO_DIM];
+
+  if (!binFile.seekSet(512)) {
+    error("binFile.seek failed");
+  }
+  uint32_t tPct = millis();
+  
+  // Prints header for csv file
+  printRecord(&csvFile, nullptr, test);
+
+  // Loop runs until user types a character or
+  // once the conversion is complete
+  while (!Serial.available() && binFile.available()) {
+    
+    // Returns the number of bytes read in binData
+    int nb = binFile.read(binData, sizeof(binData));
+    if (nb <= 0 ) {
+      error("read binFile failed");
+    }
+    // nr is the number of data_t instances logged
+    size_t nr = nb/sizeof(data_t);
+    for (size_t i = 0; i < nr; i++) {
+      printRecord(&csvFile, &binData[i], test);
+    }
+
+    if ((millis() - tPct) > 1000) {
+      uint8_t pct = binFile.curPosition()/(binFile.fileSize()/100);
+      if (pct != lastPct) {
+        tPct = millis();
+        lastPct = pct;
+        Serial.print(pct, DEC);
+        Serial.println('%');
+        csvFile.sync();
+      }
+    }
+    if (Serial.available()) {
+      break;
+    }
+  }
+  csvFile.close();
+  Serial.print(F("Done: "));
+  Serial.print(0.001*(millis() - start));
+  Serial.println(F(" Seconds"));
+}
+//-------------------------------------------------------------------------------
+void createBinFile() {
+  binFile.close();
+  while (sd.exists(binName)) {
+    char* p = strchr(binName, '.');
+    if (!p) {
+      error("no dot in filename");
+    }
+    while (true) {
+      p--;
+      if (p < binName || *p < '0' || *p > '9') {
+        error("Can't create file name");
+      }
+      if (p[0] != '9') {
+        p[0]++;
+        break;
+      }
+      p[0] = '0';
+    }
+  }
+  if (!binFile.open(binName, O_RDWR | O_CREAT)) {
+    error("open binName failed");
+  }
+  Serial.println(binName);
+  if (!binFile.preAllocate(PREALLOCATE_SIZE)) {
+    error("preAllocate failed");
+  }
+
+  Serial.print(F("preAllocated: "));
+  Serial.print(PREALLOCATE_SIZE_MiB);
+  Serial.println(F(" MiB"));
+}
+//-------------------------------------------------------------------------------
+bool createCsvFile() {
+  char csvName[FILE_NAME_DIM];
+  if (!binFile.isOpen()) {
+    Serial.println(F("No current binary file"));
+    return false;
+  }
+
+  // Create a new csvFile.
+  binFile.getName(csvName, sizeof(csvName));
+  char* dot = strchr(csvName, '.');
+  if (!dot) {
+    error("no dot in filename");
+  }
+  strcpy(dot + 1, "csv");
+  if (!csvFile.open(csvName, O_WRONLY | O_CREAT | O_TRUNC)) {
+    error("open csvFile failed");
+  }
+  serialClearInput();
+  Serial.print(F("\nCreated CSV: "));
+  Serial.println(csvName);
+  return true;
+}
+//-------------------------------------------------------------------------------
+void logData() {
+  int32_t delta;  // Jitter in log time.
+  int32_t maxDelta = 0;
+  uint32_t maxLogMicros = 0;
+  uint32_t maxWriteMicros = 0;
+  size_t maxFifoUse = 0;
+  size_t fifoCount = 0;
+  size_t fifoHead = 0;
+  size_t fifoTail = 0;
+  uint16_t overrun = 0;
+  uint16_t maxOverrun = 0;
+  uint32_t totalOverrun = 0;
+  uint32_t fifoBuf[128*FIFO_SIZE_SECTORS];
+  data_t* fifoData = (data_t*)fifoBuf;
+
+  Serial.println();
+  Serial.println(F("Hit button to start logging..."));
+  // Wait for the start button to be pushed to synchronize Teensy's
+  while(digitalRead(start_button_pin) == 0) {
+    delay(10);
+  }
+  delay(250);
+  
+  // Write dummy sector to start multi-block write.
+  dbgAssert(sizeof(fifoBuf) >= 512);
+  memset(fifoBuf, 0, sizeof(fifoBuf));
+  if (binFile.write(fifoBuf, 512) != 512) {
+    error("write first sector failed");
+  }
+  serialClearInput();
+
+  // Wait until SD is not busy.
+  while (sd.card()->isBusy()) {}
+
+  Serial.println(F("Logging. \nHit button to stop logging..."));
+  // Start time for log file.
+  uint32_t m = millis();
+
+  t0 = micros();
+  // Time to log next record.
+  uint32_t logTime = micros();
+  while (true) {
+    // Time for next data record.
+    logTime += LOG_INTERVAL_USEC;
+
+    // Wait until time to log data.
+    delta = micros() - logTime;
+    if (delta > 0) {
+      Serial.print(F("delta: "));
+      Serial.println(delta);
+      error("Rate too fast");
+    }
+    while (delta < 0) {
+      delta = micros() - logTime;
+    }
+
+    if (fifoCount < FIFO_DIM) {
+      uint32_t m = micros();
+      logRecord(fifoData + fifoHead);
+      m = micros() - m;
+      if (m > maxLogMicros) {
+        maxLogMicros = m;
+      }
+      fifoHead = fifoHead < (FIFO_DIM - 1) ? fifoHead + 1 : 0;
+      fifoCount++;
+      if (overrun) {
+        if (overrun > maxOverrun) {
+          maxOverrun = overrun;
+        }
+        overrun = 0;
+      }
+    } else {
+      totalOverrun++;
+      overrun++;
+      if (overrun > 0XFFF) {
+        error("too many overruns");
+      }
+    }
+    // Save max jitter.
+    if (delta > maxDelta) {
+      maxDelta = delta;
+    }
+    // Write data if SD is not busy.
+    if (!sd.card()->isBusy()) {
+      size_t nw = fifoHead > fifoTail ? fifoCount : FIFO_DIM - fifoTail;
+      // Limit write time by not writing more than 512 bytes.
+      const size_t MAX_WRITE = 512/sizeof(data_t);
+      if (nw > MAX_WRITE) nw = MAX_WRITE;
+      size_t nb = nw*sizeof(data_t);
+      uint32_t usec = micros();
+      if (nb != binFile.write(fifoData + fifoTail, nb)) {
+        error("write binFile failed");
+      }
+      usec = micros() - usec;
+      if (usec > maxWriteMicros) {
+        maxWriteMicros = usec;
+      }
+      fifoTail = (fifoTail + nw) < FIFO_DIM ? fifoTail + nw : 0;
+      if (fifoCount > maxFifoUse) {
+        maxFifoUse = fifoCount;
+      }
+      fifoCount -= nw;
+      
+      // If start button is pushed again, end trial
+      if (digitalRead(start_button_pin) == 1) {
+        break;
+      }
+    }
+  }
+  // Compute total log time in seconds
+  log_time = 0.001 * (millis() - m);
+
+  Serial.print(F("\nLog time: "));
+  Serial.print(log_time);
+  Serial.println(F(" Seconds"));
+  binFile.truncate();
+  binFile.sync();
+  Serial.print(("File size: "));
+  // Warning cast used for print since fileSize is uint64_t.
+  Serial.print((uint32_t)binFile.fileSize());
+  Serial.println(F(" bytes"));
+  Serial.print(F("totalOverrun: "));
+  Serial.println(totalOverrun);
+  Serial.print(F("FIFO_DIM: "));
+  Serial.println(FIFO_DIM);
+  Serial.print(F("maxFifoUse: "));
+  Serial.println(maxFifoUse);
+  Serial.print(F("maxLogMicros: "));
+  Serial.println(maxLogMicros);
+  Serial.print(F("maxWriteMicros: "));
+  Serial.println(maxWriteMicros);
+  Serial.print(F("Log interval: "));
+  Serial.print(LOG_INTERVAL_USEC);
+  Serial.print(F(" micros\nmaxDelta: "));
+  Serial.print(maxDelta);
+  Serial.println(F(" micros"));
+}
+//------------------------------------------------------------------------------
+void openBinFile() {
+  char name[FILE_NAME_DIM];
+  serialClearInput();
+  Serial.println(F("Enter file name"));
+  if (!serialReadLine(name, sizeof(name))) {
+    return;
+  }
+  if (!sd.exists(name)) {
+    Serial.println(name);
+    Serial.println(F("File does not exist"));
+    return;
+  }
+  binFile.close();
+  if (!binFile.open(name, O_RDONLY)) {
+    Serial.println(name);
+    Serial.println(F("open failed"));
+    return;
+  }
+  Serial.println(F("File opened"));
+}
+//-----------------------------------------------------------------------------
+void printData() {
+  if (!binFile.isOpen()) {
+    Serial.println(F("No current binary file"));
+    return;
+  }
+  // Skip first dummy sector.
+  if (!binFile.seekSet(512)) {
+    error("seek failed");
+  }
+  serialClearInput();
+  Serial.println(F("type any character to stop\n"));
+  delay(1000);
+  printRecord(&Serial, nullptr, test);
+  while (binFile.available() && !Serial.available()) {
+    data_t record;
+    if (binFile.read(&record, sizeof(data_t)) != sizeof(data_t)) {
+      error("read binFile failed");
+    }
+    printRecord(&Serial, &record, test);
+  }
+}
+//------------------------------------------------------------------------------
+void printUnusedStack() {
+#if HAS_UNUSED_STACK  
+  Serial.print(F("\nUnused stack: "));
+  Serial.println(UnusedStack());
+#endif  // HAS_UNUSED_STACK 
+}
+//------------------------------------------------------------------------------
+void serialClearInput() {
+  do {
+    delay(10);
+  } while (Serial.read() >= 0);
+}
+//------------------------------------------------------------------------------
+bool serialReadLine(char* str, size_t size) {
+  size_t n = 0;
+  while(!Serial.available()) {
+    yield();
+  }
+  while (true) {
+    int c = Serial.read();
+    if (c < ' ') break;
+    str[n++] = c;
+    if (n >= size) {
+      Serial.println(F("input too long"));
+      return false;
+    }
+    uint32_t m = millis();
+    while (!Serial.available() && (millis() - m) < 100){}
+    if (!Serial.available()) break;
+  }
+  str[n] = 0;
+  return true;
+}
+//------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  FillStack();
+#if !ENABLE_DEDICATED_SPI
+  Serial.println(F(
+    "\nFor best performance edit SdFatConfig.h\n"
+    "and set ENABLE_DEDICATED_SPI nonzero"));
+#endif  // !ENABLE_DEDICATED_SPI
+
+  Serial.print(FIFO_DIM);
+  Serial.println(F(" FIFO entries will be used."));
+
+  // Initialize SD.
+  if (!sd.begin(SD_CONFIG)) {
+    sd.initErrorHalt(&Serial);
+  }
+
+  setup_imus(250); // Init with the rate
+  pinMode(start_button_pin, INPUT);
+}
+//------------------------------------------------------------------------------
+void loop() {
+  printUnusedStack();
+  // Read any Serial data.
+  serialClearInput();
+
+  // For printData(), clarifying if you want to test or not (to Serial)
+  test = false;
+  
+  createBinFile();
+  // Button prompt is inside logData to allow the best sync between Teensy's
+  logData();
+  // Convert file once done.
+  if (createCsvFile())
+      binaryToCsv();
+      
+  // Show complete session
+  Serial.println(F("\nSession complete."));
+  while(true);
+}
